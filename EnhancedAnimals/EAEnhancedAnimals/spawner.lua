68b30611c63614c7aa63be3ccf7ef858e2fcce49
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2021-05-29 00:58:33 -0400

    About to test soon

--- EnhancedAnimals/EAEnhancedAnimals/spawner.lua
+++ EnhancedAnimals/EAEnhancedAnimals/spawner.lua
@@ -1,1 +1,19 @@
-
+--DEER--
+Citizen.CreateThread(function()
+    RequestModel(GetHashKey("a_m_y_business_03"))
+	
+    while not HasModelLoaded(GetHashKey("a_m_y_business_03")) do
+        Wait(1)
+    end
+	
+	if Config.Deer then
+		for _, item in pairs(Config.Locations1) do
+			local npc = CreatePed(4, 0xA1435105, item.x, item.y, item.z, item.heading, false, true)
+			
+			SetEntityHeading(npc, item.heading)
+			FreezeEntityPosition(npc, true)
+			SetEntityInvincible(npc, true)
+			SetBlockingOfNonTemporaryEvents(npc, true)
+		end
+	end
+end)
