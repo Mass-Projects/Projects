135da474f5a4c3f7a400e8eac21e3ebe164f740c
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2021-05-29 04:32:27 -0400

    Update fxmanifest.lua

--- EnhancedAnimals/EAEnhancedAnimals/fxmanifest.lua
+++ EnhancedAnimals/EAEnhancedAnimals/fxmanifest.lua
@@ -1,1 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MassProjects'
+description 'A resource that spawns animals!'
+version '0.0.1'
+
+-- What to run
+client_scripts {
+    'distancecheck.lua'
+}
+server_scripts {
+    'config.lua',
+    'spawner.lua,
+    'stream/*',
+    'animals/*'
+}
+
 
