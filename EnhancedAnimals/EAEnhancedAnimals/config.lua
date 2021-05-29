6cf4909e329caa4d72f1190cec4aebd7ec92ea21
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2021-05-29 00:33:47 -0400

    Added code to config.lua
    Added a bit of code so I can get started with this

--- EnhancedAnimals/EAEnhancedAnimals/config.lua
+++ EnhancedAnimals/EAEnhancedAnimals/config.lua
@@ -1,1 +1,31 @@
+Config = {}
 
+
+--DEFAULT GTAV ANIMALS--
+Config.Deer = true, 3.5
+Config.Boar = true, 3.5
+Config.Birds = true, 10
+
+
+--SREAMED ANIMALS (CUSTOM ONES WE MADE)--
+
+
+
+--ZONES--
+Config.Zone1 = 
+Config.Zone2 =
+Config.Zone3 =
+
+
+
+
+
+
+
+
+
+
+
+--HOW IT WORKS--
+--For Example Config.Deer = true, 3.5--
+--True being if it will spawn or not while 3.5 being the chance/rarity it will spawn--
