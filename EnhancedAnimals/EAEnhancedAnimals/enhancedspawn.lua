536bce6ae728e8d4cbe069b23dddfe8d7998a7c6
BestLeaks <59940456+BestLeaks@users.noreply.github.com>
2021-05-29 06:19:24 -0400

    Attempt #3 i think it failed!

--- EnhancedAnimals/EAEnhancedAnimals/enhancedspawn.lua
+++ EnhancedAnimals/EAEnhancedAnimals/enhancedspawn.lua
@@ -1,1 +1,20 @@
+--DEER--
+Citizen.CreateThread(function()
+  RequestModel(GetHashKey("a_c_deer"))
+  HasModelLoaded("a_c_deer") then CreatePed( 
+		modelHash GetHashKey("a_c_deer"), 
+		x --[[ number ]], 
+		y --[[ number ]], 
+		z --[[ number ]], 
+		heading --[[ number ]], 
+		isNetwork --[[ boolean ]], 
+		bScriptHostPed --[[ boolean ]]
+	)
+    Citizen.Wait(1900)
+--This is idk junk lol--
 
+    end  
+  end
+end)
+  )
+)
